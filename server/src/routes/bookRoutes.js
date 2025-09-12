141b4eb74ae9ce275eb175ced5d568177d24b50c
Jason <byronjason902@gmail.com>
2025-09-12 13:05:38 +0100

    feat: Add pagination for fetching books

--- server/src/routes/bookRoutes.js
+++ server/src/routes/bookRoutes.js
@@ -35,15 +35,30 @@ router.post("/books", protectRoute, async (req, res) => {
   }
 });
 
-
-
 router.get("/", protectRoute, async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log("Error in get all ")
-        
-    }
-})
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ creatAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks: total,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+
+    
+  } catch (error) {
+    console.log("Error in get all books route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 export default router;
